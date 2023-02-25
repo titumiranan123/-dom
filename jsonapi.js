eac108f8a30137f2bf79a3eaaf83aff1a51ef9f9
titumiranan123 <titumiranan.gtc@gmail.com>
2023-02-25 23:47:15 +0600

    d

--- jsonapi.js
+++ jsonapi.js
@@ -1,10 +1,25 @@
-fetch('https://jsonplaceholder.typicode.com/photos')
-.then(res=>res.json())
-.then(json => display(json));
+document.getElementById("dataLoad").addEventListener("click", function () {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((res) => res.json())
+    .then((json) => display(json));
+  // .then((json) => console.log(json));
+});
 
-function display(info){
-const photo =document.getElementById('photo');
-const div = document.createElement('div');
-div.innerText = info;
-photo.appendChild(div);
+function display(info) {
+    const photo = document.getElementById("photo");
+  for (const photoes of info) {
+    // console.log(photoes.thumbnailUrl);
+    const photos = photoes.thumbnailUrl;
+    const img = document.createElement("img");
+    img.innerHTML = `<img src="${photos} ">`;
+    // console.log(img);
+    // const div = document.createElement("div");
+    // div.innerText = photoes.thumbnailUrl;
+    // photo.appendChild(div);
+    photo.appendChild(img);
+  }
+  //   const photo = document.getElementById("photo");
+  //   const div = document.createElement("div");
+  //   div.innerText = info;
+  //   photo.appendChild(div);
 }
